52e457f32dc1c9c7972ab99ada076c0eb9eeb767
Juan Carlos Buruchaga <juan.buruchaga@multicaja.cl>
2019-08-09 17:17:14 -0400

    add collision validation

--- app/service/adService.js
+++ app/service/adService.js
@@ -1,4 +1,4 @@
-const {Advertisement} = require('../sequelize/sequelize');
+const {Advertisement, sequelize} = require('../sequelize/sequelize');
 
 getPendingAds = () =>{
     return new Promise((resolve, reject) => {
@@ -14,9 +14,23 @@ getPendingAds = () =>{
     });
 }
 
-updateAccount = (id, account ) =>{
+getbuyedAds = () =>{
     return new Promise((resolve, reject) => {
-        Advertisement.update({enabled:'1', owner: account},{where: {            
+        Advertisement.findAll({raw: true, where: {            
+            enabled : '1'
+          }})
+        .then((data) => { 
+            resolve(data);
+        }).catch(error => { 
+            reject(error);
+        }); 
+    });
+}
+
+
+updateAccount = (id, account, enabled ) =>{
+    return new Promise((resolve, reject) => {
+        Advertisement.update({enabled:enabled, owner: account},{where: {            
             idx : id
           }})
         .then((data) => { 
@@ -26,10 +40,31 @@ updateAccount = (id, account ) =>{
         }); 
     });
 }
+
+isCollide = (x,y,x2,y2 ) =>{
+    return new Promise((resolve, reject) => {
+        sequelize.query("select * from  million_ban.advertisement where (x >= "+x+" and x <= "+x2+" and enabled = '1') or (y >= "+y+" and y <= "+y2+" and enabled = '1') ", { type: sequelize.QueryTypes.SELECT})
+        .then(users => { 
+            console.log("select * from  million_ban.advertisement where (x >= "+x+" and x <= "+x2+" and enabled = '1') or (y >= "+y+" and y <= "+y2+" and enabled = '1') ");
+            if (users.length > 0) {
+              resolve(true);
+            }
+            else
+            {
+                resolve(false);
+            }
+        })    
+    });
+}
+
  
 
-
+// select * from  million_ban.advertisement where 
+// (x >= 90 and x <= 100) or (y >= 90 and y <= 119)
+// and enabled = '1'
 module.exports = {
     getPendingAds:getPendingAds,
-    updateAccount:updateAccount
+    updateAccount:updateAccount,
+    isCollide:isCollide,
+    getbuyedAds:getbuyedAds
 }
--- service.js
+++ service.js
@@ -2,8 +2,53 @@ var schedule = require('node-schedule');
 var service = require('./app/service/adService');
 const bananojs = require('bananojs')('http://localhost:7072');
  
+var grid = [];
+function grid_array2d(w, h) {
+   grid = [];
+   grid.length = h;
+ 
+   for (let x=0; x<100; x++) {
+     const row = [];
+     row.length = w;
+     for(let y=0; y<100; y++) row[y] = 0;
+     grid[x] = row;
+   }
+   return grid;
+}
+
+function isCollide(x1, y1, x2, y2) {
+   // Returns true if has collision, inclusive.
+   if (x1 < 0 || y1 < 0 || x2 >= 100 || y2 >= 100) return true;
+
+   for (let x=Number(x1); x<=x2; x++) {
+     for (let y=Number(y1); y<=y2; y++) {
+       if(grid[x][y]) return true;
+     }
+   }
+   return false;
+ }
+
+ function setPoints(x1, y1, x2, y2) {
+   for (let x=Number(x1); x<=x2; x++) {
+     for (let y=Number(y1); y<=y2; y++) {
+       grid[x][y] = true;
+     }
+   }
+ }
+
 var j = schedule.scheduleJob('*/5 * * * * *', async () => { 
    let ads = await service.getPendingAds(); 
+   let buyed = await service.getbuyedAds(); 
+   if (ads) {
+      grid_array2d(100,100);
+      buyed.forEach(ad => {
+         const x1 = Math.floor(ad.x/10);
+         const y1 = Math.floor(ad.y/10);
+         const x2 = (Number(ad.x)+Number(ad.width)-1)/10;
+         const y2 = (Number(ad.y)+Number(ad.height)-1)/10;
+         setPoints(x1,y1,x2,y2);
+      });
+   }
    ads.forEach( async (ad) => { 
       let a = await bananojs.getPendingBlocksForAccount(ad.generated_address,4,0,false); 
       let ban_to_deposit = ad.width * ad.height;
@@ -16,7 +61,18 @@ var j = schedule.scheduleJob('*/5 * * * * *', async () => {
            from_account = b.block_account; 
            ban_total =  parseInt(await bananojs.rawToBan(b.amount)) + ban_total; 
            if (ban_total >= ban_to_deposit) { 
-             await service.updateAccount(ad.idx,from_account );
+            const x1 = Math.floor(ad.x/10);
+            const y1 = Math.floor(ad.y/10);
+            const x2 = x1 + Math.floor(ad.width/10) - 1;
+            const y2 = y1 + Math.floor(ad.height/10) - 1;
+            if(!isCollide(x1, y1, x2, y2))
+            {
+               await service.updateAccount(ad.idx,from_account,'1' );
+            }
+            else
+            {
+               await service.updateAccount("return funds "+ad.idx,from_account,'0' );
+            }
            } 
          });
       }
